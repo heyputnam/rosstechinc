c9e79e6121a35215a9c691b86cc667551eec0bc5
Ross Tech <rosstech@Haileys-MacBook-Air.local>
2022-06-08 12:12:11 -0400

    changed colors

--- src/colors.css.ts
+++ src/colors.css.ts
@@ -1,8 +1,8 @@
 export const colors = {
-  background: "#ffd500",
-  text: "#005bbb",
-  primary: "#005bbb",
-  muted: "#f5cc00",
-  active: "#004287",
+  background: "#FF8334",
+  text: "white",
+  primary: "#5271FF",
+  muted: "white",
+  active: "#000",
   black: "#000",
 }
